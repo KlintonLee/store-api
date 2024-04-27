9db272253586da70f5f72074747ff056a673b74c
klinton lee <klintonlee92@gmail.com>
2024-04-27 19:10:28 -0300

    enhancement: add validation for null and empty Admin email

--- domain/src/main/java/com/klinton/store/domain/users/admin/AdminValidator.java
+++ domain/src/main/java/com/klinton/store/domain/users/admin/AdminValidator.java
@@ -15,10 +15,22 @@ public class AdminValidator extends Validator {
 
     @Override
     public void validate() {
+        validateName();
+        validateEmail();
+    }
+
+    private void validateName() {
         final var name = admin.getName();
 
         if (name == null || name.isBlank()) {
             validationHandler().append(new Error("Name should not be null or empty"));
+        }
+    }
+
+    private void validateEmail() {
+        final var email = admin.getEmail();
+        if (email == null || email.isBlank()) {
+            validationHandler().append(new Error("Email should not be null or empty"));
         }
     }
 }
