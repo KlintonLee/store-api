9ca4a6051667e9efae790eb6a69eb4e460cf4227
klinton lee <klintonlee92@gmail.com>
2024-04-27 15:32:06 -0300

    test: add validation for null and empty Product name

--- domain/src/test/java/com/klinton/store/domain/product/ProductTest.java
+++ domain/src/test/java/com/klinton/store/domain/product/ProductTest.java
@@ -1,5 +1,7 @@
 package com.klinton.store.domain.product;
 
+import com.klinton.store.domain.exception.DomainException;
+import com.klinton.store.domain.validation.ThrowValidationHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -27,5 +29,37 @@ public class ProductTest {
         assertNotNull(product.getCreatedAt());
         assertNotNull(product.getUpdatedAt());
         assertNull(product.getDeletedAt());
+    }
+
+    @Test
+    public void givenNullName_whenCallNewProduct_thenShouldThrownAnException() {
+        // Arrange
+        final String name = null;
+        final var description = "Product Description";
+        final var quantity = 10;
+        final var price = 100.0;
+
+        // Act
+        final var product = Product.with(name, description, quantity, price);
+        final var exception = assertThrows(DomainException.class, () -> product.validate(new ThrowValidationHandler()));
+
+        // Assert
+        assertEquals("Name should not be null or empty", exception.getMessage());
+    }
+
+    @Test
+    public void givenEmptyName_whenCallNewProduct_thenShouldThrownAnException() {
+        // Arrange
+        final var name = " ";
+        final var description = "Product Description";
+        final var quantity = 10;
+        final var price = 100.0;
+
+        // Act
+        final var product = Product.with(name, description, quantity, price);
+        final var exception = assertThrows(DomainException.class, () -> product.validate(new ThrowValidationHandler()));
+
+        // Assert
+        assertEquals("Name should not be null or empty", exception.getMessage());
     }
 }
