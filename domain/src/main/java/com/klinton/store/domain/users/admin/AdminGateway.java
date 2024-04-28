bfdf90614b9b870e418090a69671f5cc4641f3da
klinton lee <klintonlee92@gmail.com>
2024-04-28 00:09:16 -0300

    chore: create Admin aggregate gateway

---
+++ domain/src/main/java/com/klinton/store/domain/users/admin/AdminGateway.java
@@ -0,0 +1,18 @@
+package com.klinton.store.domain.users.admin;
+
+import com.klinton.store.domain.pagination.Pagination;
+import com.klinton.store.domain.pagination.SearchQuery;
+
+import java.util.Optional;
+
+public interface AdminGateway {
+    Admin create(Admin admin);
+
+    Optional<Admin> getById(AdminID adminID);
+
+    Admin update(Admin admin);
+
+    void delete(AdminID adminID);
+
+    Pagination<Admin> getAll(SearchQuery query);
+}
