aacc705868f965ff9bed17cbbc5d05e91733d002
klinton lee <klintonlee92@gmail.com>
2024-04-30 18:40:51 -0300

    chore: initial structure of spring main

--- infrastructure/src/main/java/com/klinton/store/infrastructure/Main.java
+++ infrastructure/src/main/java/com/klinton/store/infrastructure/Main.java
@@ -2,12 +2,12 @@ package com.klinton.store.infrastructure;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
-
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
+        SpringApplication.run(WebServerConfig.class, args);
     }
-
 }
---
+++ infrastructure/src/main/java/com/klinton/store/infrastructure/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.klinton.store.infrastructure;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.klinton.store")
+public class WebServerConfig {
+}
