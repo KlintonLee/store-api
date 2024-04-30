8fa25977f1aa10f4a48d346abe3c2a6f1fcfdb17
klinton lee <klintonlee92@gmail.com>
2024-04-30 03:10:22 -0300

    chore: add test for image media value object

---
+++ domain/src/test/java/com/klinton/store/domain/media/ImageMediaTest.java
@@ -0,0 +1,46 @@
+package com.klinton.store.domain.media;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ImageMediaTest {
+
+    @Test
+    public void givenValidParamsWithoutId_whenCallImageMediaWith_thenShouldReturnImageMediaInstance() {
+        // Given
+        var checksum = "checksum";
+        var name = "name";
+        var location = "location";
+
+        // When
+        var imageMedia = ImageMedia.with(checksum, name, location);
+
+        // Then
+        assertNotNull(imageMedia);
+        assertNotNull(imageMedia.id());
+        assertEquals(checksum, imageMedia.checksum());
+        assertEquals(name, imageMedia.name());
+        assertEquals(location, imageMedia.location());
+    }
+
+    @Test
+    public void givenValidParamsWithId_whenCallImageMediaWith_thenShouldReturnImageMediaInstance() {
+        // Given
+        var id = "id";
+        var checksum = "checksum";
+        var name = "name";
+        var location = "location";
+
+        // When
+        var imageMedia = ImageMedia.with(id, checksum, name, location);
+
+        // Then
+        assertNotNull(imageMedia);
+        assertEquals(id, imageMedia.id());
+        assertEquals(checksum, imageMedia.checksum());
+        assertEquals(name, imageMedia.name());
+        assertEquals(location, imageMedia.location());
+    }
+}
