891810e80e755cfeba06642fb74ad38710c372ec
klinton lee <klintonlee92@gmail.com>
2024-05-01 17:36:52 -0300

    enhancement: add validation for null and empty email

--- domain/src/main/java/com/klinton/store/domain/users/customer/CustomerValidator.java
+++ domain/src/main/java/com/klinton/store/domain/users/customer/CustomerValidator.java
@@ -17,9 +17,23 @@ public class CustomerValidator extends Validator {
 
     @Override
     public void validate() {
+        validateName();
+        validateEmail();
+    }
+
+
+
+    private void validateName() {
         var name = customer.getName();
         if (name == null || name.isBlank()) {
             this.validationHandler().append(new Error("Name should not be null or empty"));
+        }
+    }
+
+    private void validateEmail() {
+        var email = customer.getEmail();
+        if (email == null || email.isBlank()) {
+            this.validationHandler().append(new Error("Email should not be null or empty"));
         }
     }
 }
