b6937439dad9c35bb90556933b3a712fd9365e79
klinton lee <klintonlee92@gmail.com>
2024-05-01 17:34:19 -0300

    test: add validation for null and empty name

--- domain/src/test/java/com/klinton/store/domain/users/customer/CustomerTest.java
+++ domain/src/test/java/com/klinton/store/domain/users/customer/CustomerTest.java
@@ -1,29 +1,64 @@
 package com.klinton.store.domain.users.customer;
 
+import com.klinton.store.domain.exception.DomainException;
+import com.klinton.store.domain.validation.ThrowValidationHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CustomerTest {
 
+    private final static String NAME = "John Doe";
+
+    private final static String EMAIL = "johndoe@email.com";
+
+    private final static String PASSWORD = "hashed_password";
+
+    private final static String PHONE = "1234567890";
+
     @Test
     public void givenAValidParams_whenCallingCreate_thenShouldReturnACustomer() {
-        // Given
-        var name = "John Doe";
-        var email = "johndoe@email.com";
-        var password = "hashed_password";
-        var phone = "1234567890";
-        var customer = Customer.create(name, email, password, phone);
+        // Act
+        var customer = Customer.create(NAME, EMAIL, PASSWORD, PHONE);
 
+        // Assert
         assertNotNull(customer);
         assertNotNull(customer.getId());
-        assertEquals(name, customer.getName());
-        assertEquals(email, customer.getEmail());
-        assertEquals(password, customer.getPassword());
-        assertEquals(phone, customer.getPhone());
+        assertEquals(NAME, customer.getName());
+        assertEquals(EMAIL, customer.getEmail());
+        assertEquals(PASSWORD, customer.getPassword());
+        assertEquals(PHONE, customer.getPhone());
         assertTrue(customer.isActive());
         assertNotNull(customer.getCreatedAt());
         assertNotNull(customer.getUpdatedAt());
         assertNull(customer.getDeletedAt());
+    }
+
+    @Test
+    public void givenNullName_whenCallingCreate_thenShouldThrowException() {
+        // Arrange
+        String nullName = null;
+        var expectedErrorMessage = "Name should not be null or empty";
+
+        // Act
+        var customer = Customer.create(nullName, EMAIL, PASSWORD, PHONE);
+        var exception = assertThrows(DomainException.class, () -> customer.validate(new ThrowValidationHandler()));
+
+        // Assert
+        assertEquals(expectedErrorMessage, exception.getMessage());
+    }
+
+    @Test
+    public void givenEmptyName_whenCallingCreate_thenShouldThrowException() {
+        // Arrange
+        String emptyName = " ";
+        var expectedErrorMessage = "Name should not be null or empty";
+
+        // Act
+        var customer = Customer.create(emptyName, EMAIL, PASSWORD, PHONE);
+        var exception = assertThrows(DomainException.class, () -> customer.validate(new ThrowValidationHandler()));
+
+        // Assert
+        assertEquals(expectedErrorMessage, exception.getMessage());
     }
 }
