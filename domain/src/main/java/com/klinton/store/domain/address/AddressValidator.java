ec21cbb5953b196bc84748663e14e777e6b77f0a
klinton lee <klintonlee92@gmail.com>
2024-05-07 14:24:59 -0300

    refactor: segregate address validations into private methods

--- domain/src/main/java/com/klinton/store/domain/address/AddressValidator.java
+++ domain/src/main/java/com/klinton/store/domain/address/AddressValidator.java
@@ -17,26 +17,42 @@ public class AddressValidator extends Validator {
 
     @Override
     public void validate() {
+        validateStreet();
+        validateCity();
+        validateBrazilianState();
+        validateAddressNumber();
+        validateZipCode();
+    }
+
+    private void validateStreet() {
         final var street = address.getStreet();
         if (street == null || street.isBlank()) {
             validationHandler().append(new Error("Street name should not be null or empty"));
         }
+    }
 
+    private void validateCity() {
         final var city = address.getCity();
         if (city == null || city.isBlank()) {
             validationHandler().append(new Error("City name should not be null or empty"));
         }
+    }
 
+    private void validateBrazilianState() {
         final var state = address.getState();
         if (state == null) {
             validationHandler().append(new Error("Brazilian state should should not be null"));
         }
+    }
 
+    private void validateAddressNumber() {
         final var number = address.getNumber();
         if (number == null || number.isBlank()) {
             validationHandler().append(new Error("Address number should not be null or empty"));
         }
+    }
 
+    private void validateZipCode() {
         final var zipCode = address.getZipCode();
         if (zipCode == null || zipCode.isBlank()) {
             validationHandler().append(new Error("Zip code should not be null or empty"));
