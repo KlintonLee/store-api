51b70149d47aea41d704cb134b0fd2082ad5a30a
klinton lee <klintonlee92@gmail.com>
2024-05-08 02:41:55 -0300

    chore: add delete admin use case

---
+++ application/src/main/java/com/klinton/store/application/admin/delete/DefaultDeleteAdminUseCase.java
@@ -0,0 +1,25 @@
+package com.klinton.store.application.admin.delete;
+
+import com.klinton.store.application.Utils;
+import com.klinton.store.domain.core.admin.AdminGateway;
+import com.klinton.store.domain.core.admin.AdminID;
+
+import java.util.Objects;
+
+public class DefaultDeleteAdminUseCase extends DeleteAdminUseCase {
+
+    private final AdminGateway adminGateway;
+
+    public DefaultDeleteAdminUseCase(final AdminGateway adminGateway) {
+        this.adminGateway = Objects.requireNonNull(adminGateway);
+    }
+
+    @Override
+    public void execute(String input) {
+        final var adminID = AdminID.from(input);
+        final var admin = adminGateway.getById(adminID).orElseThrow(Utils.notFound(adminID, AdminID.class));
+
+        admin.update(admin.getName(), admin.getEmail(), admin.getPassword(), false);
+        adminGateway.update(admin);
+    }
+}
---
+++ application/src/main/java/com/klinton/store/application/admin/delete/DeleteAdminUseCase.java
@@ -0,0 +1,6 @@
+package com.klinton.store.application.admin.delete;
+
+import com.klinton.store.application.UnitUseCase;
+
+public abstract class DeleteAdminUseCase extends UnitUseCase<String> {
+}
