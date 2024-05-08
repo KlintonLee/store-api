fdaacd79cad78526dac2327a7d7d5a2644ded913
klinton lee <klintonlee92@gmail.com>
2024-05-08 02:24:55 -0300

    chore: add notification pattern to admin creation

--- application/src/main/java/com/klinton/store/application/admin/create/DefaultCreateAdminUseCase.java
+++ application/src/main/java/com/klinton/store/application/admin/create/DefaultCreateAdminUseCase.java
@@ -2,8 +2,12 @@ package com.klinton.store.application.admin.create;
 
 import com.klinton.store.domain.core.admin.Admin;
 import com.klinton.store.domain.core.admin.AdminGateway;
+import com.klinton.store.domain.exception.UnprocessableEntityException;
+import com.klinton.store.domain.validation.Error;
+import com.klinton.store.domain.validation.Notification;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class DefaultCreateAdminUseCase extends CreateAdminUseCase {
 
@@ -20,8 +24,22 @@ public class DefaultCreateAdminUseCase extends CreateAdminUseCase {
         final var password = command.password();
         final var active = command.active();
 
-        final var admin = adminGateway.create(Admin.create(name, email, password, active));
+        final var notification = Notification.create();
+        final var admin = Admin.create(name, email, password, active);
+        admin.validate(notification);
 
+        if (notification.hasError()) {
+            unprocessableEntity(notification);
+        }
+        adminGateway.create(admin);
         return CreateAdminOutput.from(admin);
+    }
+
+    private static void unprocessableEntity(Notification notification) {
+        final var errorMessage = notification.getErrors()
+                .stream()
+                .map(Error::message)
+                .collect(Collectors.joining("; "));
+        throw new UnprocessableEntityException(errorMessage);
     }
 }
---
+++ domain/src/main/java/com/klinton/store/domain/exception/UnprocessableEntityException.java
@@ -0,0 +1,8 @@
+package com.klinton.store.domain.exception;
+
+public class UnprocessableEntityException extends DomainException {
+
+    public UnprocessableEntityException(String message) {
+        super(message);
+    }
+}
