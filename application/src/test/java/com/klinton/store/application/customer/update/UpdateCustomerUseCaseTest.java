fc0b23965e336fe5c5aafd3eb04a0c58efe327c1
klinton lee <klintonlee92@gmail.com>
2024-05-12 03:55:41 -0300

    test: add more update customer use cases test scenarios

--- application/src/test/java/com/klinton/store/application/customer/update/UpdateCustomerUseCaseTest.java
+++ application/src/test/java/com/klinton/store/application/customer/update/UpdateCustomerUseCaseTest.java
@@ -2,6 +2,8 @@ package com.klinton.store.application.customer.update;
 
 import com.klinton.store.domain.core.customer.Customer;
 import com.klinton.store.domain.core.customer.CustomerGateway;
+import com.klinton.store.domain.exception.NotFoundException;
+import com.klinton.store.domain.exception.UnprocessableEntityException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,7 +13,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.Objects;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
 import static org.mockito.Mockito.*;
 
@@ -66,5 +68,77 @@ public class UpdateCustomerUseCaseTest {
                 updatedAt.isBefore(aCustomer.getUpdatedAt()) &&
                 aCustomer.getDeletedAt() != null
         ));
+    }
+
+    @Test
+    public void givenAnInvalidCommandWithNullName_whenCallUpdateCustomerUseCase_shouldThrowException() {
+        // Arrange
+        final var customer = Customer.create("JANE DOE", "jane.doe@fake_email.com", "654321", "987654321");
+        final var customerId = customer.getId();
+        final var expectedErrorMessage = "Name should not be null or empty";
+
+        final var command = UpdateCustomerCommand.with(
+                customerId.getValue(),
+                null,
+                EXPECTED_EMAIL,
+                EXPECT_PASSWORD,
+                EXPECT_PHONE,
+                false
+        );
+
+        when(customerGateway.getById(customer.getId())).thenReturn(Optional.of(customer));
+
+        // Act
+        final var exception = assertThrows(UnprocessableEntityException.class, () -> useCase.execute(command));
+
+        // Assert
+        assertEquals(expectedErrorMessage, exception.getMessage());
+    }
+
+    @Test
+    public void givenACommandWithNonExistingCustomer_whenCallUpdateCustomerUseCase_shouldThrowNotFoundException() {
+        // Arrange
+        final var customerId = "invalid_id";
+        final var expectedErrorMessage = "Customer with ID invalid_id was not found.";
+        final var command = UpdateCustomerCommand.with(
+                customerId,
+                EXPECTED_NAME,
+                EXPECTED_EMAIL,
+                EXPECT_PASSWORD,
+                EXPECT_PHONE,
+                false
+        );
+
+        // Act
+        final var exception = assertThrows(NotFoundException.class, () -> useCase.execute(command));
+
+        // Assert
+        assertEquals(expectedErrorMessage, exception.getMessage());
+    }
+
+    @Test
+    public void givenAValidCommand_whenGatewayThrowsException_shouldReturnTheException() {
+        // Arrange
+        final var customer = Customer.create("JANE DOE", "jane.doe@fake_email.com", "654321", "987654321");
+        final var customerId = customer.getId();
+        final var command = UpdateCustomerCommand.with(
+                customerId.getValue(),
+                EXPECTED_NAME,
+                EXPECTED_EMAIL,
+                EXPECT_PASSWORD,
+                EXPECT_PHONE,
+                false
+        );
+
+        final var expectedErrorMessage = "Gateway error";
+
+        when(customerGateway.getById(customer.getId())).thenReturn(Optional.of(customer));
+        when(customerGateway.save(any())).thenThrow(new IllegalStateException(expectedErrorMessage));
+
+        // Act
+        final var exception = assertThrows(IllegalStateException.class, () -> useCase.execute(command));
+
+        // Assert
+        assertEquals(expectedErrorMessage, exception.getMessage());
     }
 }
