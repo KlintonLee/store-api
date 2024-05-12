cc936d3ed4ef443b4a55107d6406be4b4df2d275
klinton lee <klintonlee92@gmail.com>
2024-05-12 15:48:05 -0300

    chore: add factory method to create customer command

--- application/src/main/java/com/klinton/store/application/customer/create/CreateCustomerCommand.java
+++ application/src/main/java/com/klinton/store/application/customer/create/CreateCustomerCommand.java
@@ -6,4 +6,12 @@ public record CreateCustomerCommand(
         String password,
         String phone
 ) {
+    public static CreateCustomerCommand with(
+            String name,
+            String email,
+            String password,
+            String phone
+    ) {
+        return new CreateCustomerCommand(name, email, password, phone);
+    }
 }
