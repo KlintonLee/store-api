ad20d6bf711136055fed76342976486cb91ee937
klinton lee <klintonlee92@gmail.com>
2024-05-12 04:05:19 -0300

    test: add list customers use case

---
+++ application/src/test/java/com/klinton/store/application/customer/retrieve/list/ListCustomersUseCaseTest.java
@@ -0,0 +1,52 @@
+package com.klinton.store.application.customer.retrieve.list;
+
+import com.klinton.store.domain.core.customer.Customer;
+import com.klinton.store.domain.core.customer.CustomerGateway;
+import com.klinton.store.domain.pagination.Pagination;
+import com.klinton.store.domain.pagination.SearchQuery;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ListCustomersUseCaseTest {
+
+    @InjectMocks
+    private DefaultListCustomersUseCase useCase;
+
+    @Mock
+    private CustomerGateway customerGateway;
+
+    @Test
+    public void givenAValidSearchQuery_whenCallListCustomersUseCase_shouldReturnAListOfCustomers() {
+        // Arrange
+        final var expectedItemsCount = 2;
+        final var expectedPage = 1;
+        final var expectedPerPage = 10;
+        final var expectedResult = Pagination.of(1, 10, 2, List.of(
+                Customer.create("Jane Doe", "jane.doe@fake_email.com", "654321", "123456789"),
+                Customer.create("John Doe", "john.doe@fake_email.com", "123456", "987654321")
+        ));
+        final var query = SearchQuery.of(expectedPage, expectedPerPage, "", "name", "asc");
+        when(customerGateway.getAll(query)).thenReturn(expectedResult);
+
+        final var expectedCustomers = expectedResult.items().stream().map(ListCustomerOutput::from).toList();
+
+        // Act
+        final var output = useCase.execute(query);
+
+        // Assert
+        Assertions.assertEquals(expectedItemsCount, output.total());
+        Assertions.assertEquals(expectedCustomers, output.items());
+        Assertions.assertEquals(expectedPage, output.currentPage());
+        Assertions.assertEquals(expectedPerPage, output.perPage());
+    }
+
+}
